3827bf6a058558958bbbbd8a7a02fb5b2fb7cb89
thuong.tran1 <thuong.tran1@ntq-solution.com.vn>
2026-07-09 14:28:01 +0700

    ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -10,8 +11,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
