e4a550d8c028d6794a64b730516ee1e004a41a24
thuong.tran1 <thuong.tran1@ntq-solution.com.vn>
2026-07-14 17:02:31 +0700

    feat: front end setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1 className='text-red-500'>Login Page</h1>
+        </div>
+    )
+}
+
+export default LoginPage
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUpPage = () => {
+    return (
+        <div>
+            <h1 className='text-red-500'>Sign Up Page</h1>
+        </div>
+    )
+}
+
+export default SignUpPage
