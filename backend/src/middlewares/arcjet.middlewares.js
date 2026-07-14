e26f7ddaa291b354d61da089cd649d5b8d59c75e
thuong.tran1 <thuong.tran1@ntq-solution.com.vn>
2026-07-14 09:38:06 +0700

    feat: message routes

--- backend/src/middlewares/arcjet.middlewares.js
+++ backend/src/middlewares/arcjet.middlewares.js
@@ -1,37 +1,33 @@
-
-import { isSpoofedBot } from "@arcjet/inspect";
 import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
 
 export const arcjetProtection = async (req, res, next) => {
     try {
         const decision = await aj.protect(req);
+
         if (decision.isDenied()) {
             if (decision.reason.isRateLimit()) {
-                res.writeHead(429, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ error: "Too Many Requests" }));
+                return res.status(429).json({ message: "Rate limit exceeded. Please try again later." });
             } else if (decision.reason.isBot()) {
-                res.writeHead(403, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ error: "No bots allowed" }));
+                return res.status(403).json({ message: "Bot access denied." });
             } else {
-                res.writeHead(403, { "Content-Type": "application/json" });
-                res.end(JSON.stringify({ error: "Forbidden" }));
+                return res.status(403).json({
+                    message: "Access denied by security policy.",
+                });
             }
-        } else if (decision.ip.isHosting()) {
-            res.writeHead(403, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Forbidden" }));
-        } else if (decision.results.some(isSpoofedBot)) {
-            res.writeHead(403, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Forbidden" }));
-        } else {
-            res.writeHead(200, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ message: "Hello World" }));
         }
+
+        // check for spoofed bots
+        if (decision.results.some(isSpoofedBot)) {
+            return res.status(403).json({
+                error: "Spoofed bot detected",
+                message: "Malicious bot activity detected.",
+            });
+        }
+
         next();
     } catch (error) {
-        console.error("Arcjet middleware error:", error);
-        res.writeHead(500, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Internal Server Error" }));
+        console.log("Arcjet Protection Error:", error);
         next();
     }
-
-}
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,16 @@
 
 import express from 'express';
+import { getAllContacts, getMessagsByUserId, sendMessage, getChatPartners } from '../controllers/message.controller.js';
+import { protectRoute } from '../middlewares/auth.middlewares.js';
 
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-    res.send('Send Message API');
-});
+
+router.use(protectRoute); // Apply the protectRoute middleware to all routes in this router
+router.get('/contact', getAllContacts);
+router.get('/chat', getChatPartners);
+router.get('/:id', getMessagsByUserId);
+router.post("/send/:id", sendMessage);
 
 
 export default router;
