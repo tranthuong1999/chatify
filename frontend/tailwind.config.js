2bb643578be4cbffdda6fdeb057656c52351ff75
Thuong Tran Van (SDC11) <thuong.tran1@ntq-solution.com.vn>
2026-07-16 14:56:20 +0700

    fix: fix aniamtion

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,16 +4,7 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        border: "border 4s linear infinite",
-      },
-      keyframes: {
-        border: {
-          to: { "--border-angle": "360deg" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [daisyui],
 };
